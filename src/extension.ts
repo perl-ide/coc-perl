7dc85572b3907c95afccce752a4b85179feef4e0
Bruno Meneguele <bmeneg@heredoc.io>
2023-07-06 18:28:27 -0300

    extension: add missing bits for 2.4.0 and warn DAP
    Some configuration options were not being handled as needed for 2.4.0
    version, like the ability of using Perl::LanguageServer from a container of
    passing envvars to the client. This commit adds these missing bits.
    
    Also, this commit add notes about the lack of DAP support on coc-nvim,
    ensureing users are aware of it.

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,29 @@
+/* AUTHOR NOTES
+ *
+ * Debug Adapter Protocol:
+ *
+ * DAP is not supported by coc-nvim like it's on VSCode. Because of that,
+ * this extension code lacks the parts of the official VSCode extension code
+ * related to DAP, however, some configuration options related to DAP are
+ * still present, but are basically NOP for coc-nvim. These will be marked
+ * with a comment containing 'DAP-SUPPORT'.
+ *
+ * You might ask why to keep DAP code partially ported? We want to keep the
+ * ability for a user to use the VSCode configuration file as-is, thus we
+ * need to keep track of some variables (even though NOP on coc-nvim) so no
+ * errors are thrown at runtime. We could keep track of DAP-related vars and
+ * ignore/remove them at runtime, but keep the code similar to the original
+ * extension prevent us, maintainer, from pulling my hear some times.
+ *
+ * Bruno Meneguele <bmeneg@heredoc.io> 2023
+ */
+
 'use strict';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import * as net from 'net';
 import {
+  window,
   workspace,
   ExtensionContext,
   LanguageClient,
@@ -18,9 +39,13 @@ interface IPerlConfig {
   perlArgs: string[];
   perlInc: string[];
 
+  // eslint-disable-next-line
+  env: any;
+
   logFile: string;
   logLevel: number;
 
+  // DAP-SUPPORT
   debugAdapterPort: number;
   debugAdapterPortRange: number;
 
@@ -29,6 +54,11 @@ interface IPerlConfig {
   sshUser: string;
   sshAddr: string;
   sshPort: number;
+
+  containerCmd: string;
+  containerArgs: string[];
+  containerName: string;
+  containerMode: string;
 }
 
 // Default values for every extension config option.
@@ -39,15 +69,20 @@ const defaultPerlConfig: IPerlConfig = {
   perlCmd: 'perl',
   perlArgs: [],
   perlInc: [],
+  env: {},
   logFile: '',
   logLevel: 0,
-  debugAdapterPort: 13603,
-  debugAdapterPortRange: 100,
+  debugAdapterPort: 13603, // DAP-SUPPORT
+  debugAdapterPortRange: 100, // DAP-SUPPORT
   sshCmd: 'ssh',
   sshArgs: [],
   sshUser: '',
   sshAddr: '',
   sshPort: 0,
+  containerCmd: '',
+  containerArgs: [],
+  containerName: '',
+  containerMode: 'exec',
 };
 
 function getConfig(): IPerlConfig {
@@ -65,6 +100,7 @@ function getConfig(): IPerlConfig {
   return config;
 }
 
+// DAP-SUPPORT
 function checkPort(port: number): Promise<number> {
   return new Promise((resolve, reject) => {
     const server = net.createServer();
@@ -79,6 +115,7 @@ function checkPort(port: number): Promise<number> {
   });
 }
 
+// DAP-SUPPORT
 async function getAvailablePort(
   port: number,
   port_range: number
@@ -104,32 +141,70 @@ async function getAvailablePort(
   return port;
 }
 
+function resolveWorkspaceFolder(path: string, resource?: string): string {
+  if (path.includes('${workspaceFolder}')) {
+    const ws =
+      workspace.getWorkspaceFolder(resource as string) ??
+      workspace.workspaceFolders?.[0];
+    const sub = ws?.uri ?? '';
+    return path.replace('${workspaceFolder}', sub);
+  }
+  return path;
+}
+
+function buildContainerArgs(
+  containerCmd: string,
+  containerArgs: string[],
+  containerName: string,
+  containerMode: string
+): string[] {
+  if (containerMode != 'exec') containerMode = 'run';
+
+  if (containerCmd) {
+    if (containerArgs.length == 0) {
+      if (containerCmd == 'docker') {
+        containerArgs.push(containerMode);
+        if (containerMode == 'run') containerArgs.push('--rm');
+        containerArgs.push('-i', containerName);
+      } else if (containerCmd == 'docker-compose') {
+        containerArgs.push(containerMode);
+        if (containerMode == 'run') containerArgs.push('--rm');
+        containerArgs.push('--no-deps', '-T', containerName);
+      } else if (containerCmd == 'kubectl') {
+        containerArgs.push('exec', containerName, '-i', '--');
+      } else if (containerCmd == 'devspace') {
+        containerArgs.push('--silent ', 'enter');
+        if (containerName) containerArgs.push('-c', containerName);
+        containerArgs.push('--');
+      }
+    }
+  }
+
+  return containerArgs;
+}
+
 export async function activate(context: ExtensionContext) {
   const config = getConfig();
   if (!config.enable) {
     console.log('extension for Perl disabled');
     return;
   }
+  console.log('extension "perl" is now active');
 
   const lsVersion = '2.4.0';
-  const perlIncOpt = config.perlInc.map((incDir: string) => '-I' + incDir);
-  // Even though the user might have chosen a fixed port, we must run
-  // through the range in case it's already in use.
-  const debugAdapterPort = await getAvailablePort(
-    config.debugAdapterPort,
-    config.debugAdapterPortRange
-  );
-  console.log('use ' + debugAdapterPort + ' as debug adapter port');
+  const resource = window.activeTextEditor?.document.uri;
+  const perlIncOpt = config.perlInc.map((incDir: string): string => {
+    return '-I' + resolveWorkspaceFolder(incDir, resource);
+  });
+  const perlCmd = resolveWorkspaceFolder(config.perlCmd, resource);
 
-  const perlArgsOpt = [
+  const perlArgsOpt: string[] = [
     ...perlIncOpt,
     ...config.perlArgs,
     '-MPerl::LanguageServer',
     '-e',
     'Perl::LanguageServer::run',
     '--',
-    '--port',
-    debugAdapterPort.toString(),
     '--log-level',
     config.logLevel.toString(),
     '--log-file',
@@ -138,12 +213,30 @@ export async function activate(context: ExtensionContext) {
     lsVersion,
   ];
 
+  // DAP-SUPPORT
+  let debugAdapterPort = config.debugAdapterPort;
+  if (!config.containerCmd) {
+    debugAdapterPort = await getAvailablePort(
+      config.debugAdapterPort,
+      config.debugAdapterPortRange
+    );
+    console.log('use ' + debugAdapterPort + ' as debug adapter port');
+    perlArgsOpt.push('--port', debugAdapterPort.toString());
+  }
+
   let sshCmd = config.sshCmd;
   let sshPortOpt = '-p';
   if (/^win/.test(process.platform)) {
     sshCmd = 'plink';
     sshPortOpt = '-P';
   }
+
+  const containerArgsOpt: string[] = buildContainerArgs(
+    config.containerCmd,
+    config.containerArgs,
+    config.containerName,
+    config.containerMode
+  );
 
   let serverCmd: string;
   let serverArgs: string[] = [];
@@ -157,19 +250,35 @@ export async function activate(context: ExtensionContext) {
       config.sshUser,
       config.sshAddr,
       '-L',
-      config.debugAdapterPort + ':127.0.0.1:' + config.debugAdapterPort,
-      config.perlCmd
+      config.debugAdapterPort + ':127.0.0.1:' + config.debugAdapterPort // DAP-SUPPORT
     );
-    serverArgs = serverArgs.concat(perlArgsOpt);
+    if (config.containerCmd) {
+      serverArgs.push(config.containerCmd);
+      serverArgs = serverArgs.concat(containerArgsOpt);
+    }
+    serverArgs = serverArgs.concat(perlCmd, perlArgsOpt);
   } else {
-    serverCmd = config.perlCmd;
-    serverArgs = perlArgsOpt;
+    if (config.containerCmd) {
+      serverCmd = config.containerCmd;
+      serverArgs = containerArgsOpt.concat(perlCmd, perlArgsOpt);
+    } else {
+      serverCmd = perlCmd;
+      serverArgs = perlArgsOpt;
+    }
   }
   console.log('cmd: ' + serverCmd + ' args: ' + serverArgs.join(' '));
 
   const serverOptions: ServerOptions = {
-    run: { command: serverCmd, args: serverArgs },
-    debug: { command: serverCmd, args: serverArgs.concat(['--debug']) },
+    run: {
+      command: serverCmd,
+      args: serverArgs,
+      options: { env: config.env },
+    },
+    debug: {
+      command: serverCmd,
+      args: serverArgs.concat(['--debug']),
+      options: { env: config.env },
+    },
   };
 
   // Options to control the language client
